c75c366e4064ae96ae8f2486388813dc943789b7
Scott Byrne <scott.byrne@forestry.io>
2020-08-26 16:57:06 -0300

    new prototype layout setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ import {
   ReactFullLogo,
   StrapiLogo,
 } from 'components/logos'
-import TinaLogomarkSvg from 'public/svg/tina-logomark.svg'
+import TinaIconSvg from 'public/svg/tina-icon.svg'
 import WhyTinaBackground from 'public/svg/why-tina-background.svg'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import codeTheme from 'components/styles/CodeHome'
@@ -260,189 +260,56 @@ const HomePage = (props: any) => {
   return (
     <InlineForm form={form}>
       <div className={styles.pageWrapper}>
-        <div className={styles.header}>
-          <a href="/">
-            <h1>
-              <TinaLogomarkSvg />
-            </h1>
-          </a>
-          <iframe
-            src="https://ghbtns.com/github-btn.html?user=tinacms&repo=tinacms&type=star&count=true&size=large"
-            frameBorder="0"
-            scrolling="0"
-            width="150px"
-            height="40px"
-          ></iframe>
-        </div>
-        <div className={styles.heroWrapper}>
-          <div className={styles.heroContent}>
-            <h1 className={styles.heroTitle}>
-              Bring Editing to Your
-              <Framework activeLibraries={activeLibraries} />
-              Project
-            </h1>
-            <p className={styles.heroText}>Made for developers and editors</p>
-            <button className={styles.heroButton}>Start Learning</button>
-          </div>
-          <div className={styles.hero}>
-            <div className={styles.wrapper}>
-              <div className={styles.aspectRatio}>
-                <div className={`${styles.grid} ${styles.gridLeft}`}>
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={1}
-                    library={Bootstrap}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={2}
-                    library={Strapi}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={3}
-                    library={Gatsby}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={4}
-                    library={Github}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={5}
-                    library={ReactLib}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={6}
-                    library={Next}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={7}
-                    library={Contentful}
-                  />
-                </div>
-                {/* End First Grid */}
-                <div className={styles.grid}>
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={1}
-                    library={Github}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={2}
-                    library={ReactLib}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={3}
-                    library={Next}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={4}
-                    library={Contentful}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={5}
-                    library={Strapi}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={6}
-                    library={Bootstrap}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={7}
-                    library={Gatsby}
-                  />
-                </div>
-                {/* End Second Grid */}
-                <div className={`${styles.grid} ${styles.gridRight}`}>
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={1}
-                    library={ReactLib}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={2}
-                    library={Github}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={3}
-                    library={Bootstrap}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={4}
-                    library={Gatsby}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={5}
-                    library={Strapi}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={6}
-                    library={Next}
-                  />
-                  <GridItem
-                    activeLibraries={activeLibraries}
-                    number={7}
-                    library={Contentful}
-                  />
-                </div>
-                {/* End Third Grid */}
-              </div>
-              {/* End Aspect Ratio */}
+        <div className={styles.heroGrid}>
+          <div className={styles.heroGridContent}>
+            <div className={styles.heroContent}>
+              <TinaIconSvg />
+              <h1 className={styles.heroTitle}>
+                Bring Editing to Your Project
+              </h1>
+              <p className={styles.heroText}>Made for developers and editors</p>
+              <button className={styles.heroButton}>Start Learning</button>
             </div>
-            {/* End End Wrapper */}
-          </div>
-          {/* End Hero*/}
-          <div className={styles.heroFeatures}>
-            <div className={styles.heroFeaturesContainer}>
-              <div
-                className={styles.heroFeatureCard}
-                onMouseEnter={setDataLibraries}
-                onMouseLeave={resetLibraries}
-              >
-                <h2>Data Sources</h2>
-                <p>
-                  Tina can fetch data anywhere your data lives, from traditional
-                  headless CMSes to Airtable and Google Sheets
-                </p>
-              </div>
-              <div
-                className={styles.heroFeatureCard}
-                onMouseEnter={setFrameworkLibraries}
-                onMouseLeave={resetLibraries}
-              >
-                <h2>Frontend Frameworks</h2>
-                <p>
-                  Tina plays well with any React-based framework. Whether your
-                  site is powered by Gatsby or Next.js, Tina has your back.
-                </p>
-              </div>
-              <div
-                className={styles.heroFeatureCard}
-                onMouseEnter={setUiLibraries}
-                onMouseLeave={resetLibraries}
-              >
-                <h2>Component Libraries</h2>
-                <p>
-                  Supercharge your website by converting your components into
-                  editable blocks.
-                </p>
+            <div className={styles.heroFeatures}>
+              <div className={styles.heroFeaturesContainer}>
+                <div
+                  className={styles.heroFeatureCard}
+                  onMouseEnter={setDataLibraries}
+                  onMouseLeave={resetLibraries}
+                >
+                  <h2>Data Sources</h2>
+                  <p>
+                    Tina can fetch data anywhere your data lives, from
+                    traditional headless CMSes to Airtable and Google Sheets
+                  </p>
+                </div>
+                <div
+                  className={styles.heroFeatureCard}
+                  onMouseEnter={setFrameworkLibraries}
+                  onMouseLeave={resetLibraries}
+                >
+                  <h2>Frontend Frameworks</h2>
+                  <p>
+                    Tina plays well with any React-based framework. Whether your
+                    site is powered by Gatsby or Next.js, Tina has your back.
+                  </p>
+                </div>
+                <div
+                  className={styles.heroFeatureCard}
+                  onMouseEnter={setUiLibraries}
+                  onMouseLeave={resetLibraries}
+                >
+                  <h2>Component Libraries</h2>
+                  <p>
+                    Supercharge your website by converting your components into
+                    editable blocks.
+                  </p>
+                </div>
               </div>
             </div>
+          </div>
+          <div className={styles.heroGridGraphic}>
+            <h1>Test</h1>
           </div>
         </div>
         {/* End Hero Wrapper*/}
@@ -553,61 +420,6 @@ const formOptions = {
       ],
     },
   ],
-}
-
-const Framework = ({ activeLibraries }) => {
-  const [animationState, setAnimationState] = React.useState(null)
-  const [activeLibrary, setActiveLibrary] = React.useState(
-    DefaultLibraries.filter(library => {
-      return library.type === 'framework'
-    })[0]
-  )
-  const [frameworkWidth, setFrameworkWidth] = React.useState(200)
-  const frameworkSpanRef = React.useRef(null)
-
-  React.useEffect(() => {
-    if (
-      !frameworkSpanRef.current ||
-      !activeLibraries.filter(library => {
-        return library.type === 'framework'
-      })[0] ||
-      activeLibraries.filter(library => {
-        return library.type === 'framework'
-      })[0] === activeLibrary
-    ) {
-      return
-    }
-
-    setAnimationState(styles.heroTitleAnimateOut)
-
-    let animateOutTime = 500
-
-    setTimeout(() => {
-      setActiveLibrary(
-        activeLibraries.filter(library => {
-          return library.type === 'framework'
-        })[0]
-      )
-      setAnimationState(styles.heroTitleAnimate)
-    }, animateOutTime)
-  }, [frameworkSpanRef.current, activeLibraries])
-
-  React.useEffect(() => {
-    if (!frameworkSpanRef.current) return
-
-    setFrameworkWidth(frameworkSpanRef.current.offsetWidth)
-  }, [frameworkSpanRef.current, activeLibrary])
-
-  return (
-    <span
-      className={`${styles.heroTitleFramework} ${styles.heroTitleFrameworkHide2}`}
-      style={{ width: `${frameworkWidth}px` }}
-    >
-      <span className={animationState} ref={frameworkSpanRef}>
-        {activeLibrary && <activeLibrary.component />}
-      </span>
-    </span>
-  )
 }
 
 interface GridItemProps {
