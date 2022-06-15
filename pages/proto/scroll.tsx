48490ee1e5f12f07294a12af92aa5fcc8aa0e77f
Scott Byrne <scott.byrne@forestry.io>
2022-06-15 15:41:10 -0300

    scroll behaviour

--- pages/proto/scroll.tsx
+++ pages/proto/scroll.tsx
@@ -1,127 +1,162 @@
-const Page = props => {
+import * as React from 'react'
+import { useInView } from 'react-intersection-observer'
+
+const data = [
+  {
+    id: '1',
+    title: 'Really Important Super Awesome Headline Text',
+    description:
+      'Phasellus quis nibh scelerisque, cursus magna a, semper mauris. Pellentesque dui eros, lobortis quis dui eu, finibus pellentesque dui.',
+  },
+  {
+    id: '2',
+    title: 'Really Important Super Awesome Headline Text',
+    description:
+      'Phasellus quis nibh scelerisque, cursus magna a, semper mauris. Pellentesque dui eros, lobortis quis dui eu, finibus pellentesque dui.',
+  },
+  {
+    id: '3',
+    title: 'Really Important Super Awesome Headline Text',
+    description:
+      'Phasellus quis nibh scelerisque, cursus magna a, semper mauris. Pellentesque dui eros, lobortis quis dui eu, finibus pellentesque dui.',
+  },
+  {
+    id: '4',
+    title: 'Really Important Super Awesome Headline Text',
+    description:
+      'Phasellus quis nibh scelerisque, cursus magna a, semper mauris. Pellentesque dui eros, lobortis quis dui eu, finibus pellentesque dui.',
+  },
+]
+
+const Feature = ({ setActiveId, item }) => {
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0.33,
+  })
+
+  React.useEffect(() => {
+    if (inView) {
+      setActiveId(item.id)
+    }
+  }, [inView])
+
   return (
     <>
-      <div className="wrapper">
-        <div className="container">
-          <div className="left">
-            <div className="feature">
-              <h2>Really Important Super Awesome Headline Text</h2>
-              <p>
-                Phasellus quis nibh scelerisque, cursus magna a, semper mauris.
-                Pellentesque dui eros, lobortis quis dui eu, finibus
-                pellentesque dui.
-              </p>
-            </div>
-            <div className="feature">
-              <h2>Really Important Super Awesome Headline Text</h2>
-              <p>
-                Phasellus quis nibh scelerisque, cursus magna a, semper mauris.
-                Pellentesque dui eros, lobortis quis dui eu, finibus
-                pellentesque dui.
-              </p>
-            </div>
-            <div className="feature">
-              <h2>Really Important Super Awesome Headline Text</h2>
-              <p>
-                Phasellus quis nibh scelerisque, cursus magna a, semper mauris.
-                Pellentesque dui eros, lobortis quis dui eu, finibus
-                pellentesque dui.
-              </p>
-            </div>
-            <div className="feature">
-              <h2>Really Important Super Awesome Headline Text</h2>
-              <p>
-                Phasellus quis nibh scelerisque, cursus magna a, semper mauris.
-                Pellentesque dui eros, lobortis quis dui eu, finibus
-                pellentesque dui.
-              </p>
-            </div>
-          </div>
-          <div className="right">
-            <div className="preview-wrapper">
-              <div className="preview"></div>
-            </div>
-          </div>
+      <div ref={ref} className={`feature ${inView ? 'visible' : ''}`}>
+        <h2>{item.title}</h2>
+        <p>{item.description}</p>
+      </div>
+      <style jsx>{`
+        .feature {
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          align-items: stretch;
+          min-height: 100vh;
+          padding: 32px 0;
+          opacity: 0.3;
+          transition: opacity 0.5s ease-in-out;
+        }
+
+        .visible {
+          opacity: 1;
+        }
+
+        @media (min-width: 1200px) {
+          .feature {
+            justify-content: center;
+          }
+        }
+
+        h2 {
+          font-size: 54px;
+          line-height: 1.25;
+          font-weight: 600;
+          margin-bottom: 32px;
+        }
+
+        p {
+          font-size: 22px;
+          line-height: 1.8;
+          font-weight: 400;
+        }
+      `}</style>
+    </>
+  )
+}
+
+const Story = () => {
+  const [activeId, setActiveId] = React.useState(null)
+
+  return (
+    <>
+      <div className="container">
+        <div className="left">
+          {data.map(item => (
+            <Feature setActiveId={setActiveId} item={item} />
+          ))}
         </div>
-        <div className="other-section"></div>
-        <div className="container">
-          <div className="left">
-            <div className="feature">
-              <h2>Really Important Super Awesome Headline Text</h2>
-              <p>
-                Phasellus quis nibh scelerisque, cursus magna a, semper mauris.
-                Pellentesque dui eros, lobortis quis dui eu, finibus
-                pellentesque dui.
-              </p>
-            </div>
-            <div className="feature">
-              <h2>Really Important Super Awesome Headline Text</h2>
-              <p>
-                Phasellus quis nibh scelerisque, cursus magna a, semper mauris.
-                Pellentesque dui eros, lobortis quis dui eu, finibus
-                pellentesque dui.
-              </p>
-            </div>
-            <div className="feature">
-              <h2>Really Important Super Awesome Headline Text</h2>
-              <p>
-                Phasellus quis nibh scelerisque, cursus magna a, semper mauris.
-                Pellentesque dui eros, lobortis quis dui eu, finibus
-                pellentesque dui.
-              </p>
-            </div>
-            <div className="feature">
-              <h2>Really Important Super Awesome Headline Text</h2>
-              <p>
-                Phasellus quis nibh scelerisque, cursus magna a, semper mauris.
-                Pellentesque dui eros, lobortis quis dui eu, finibus
-                pellentesque dui.
-              </p>
-            </div>
-          </div>
-          <div className="right">
-            <div className="preview-wrapper">
-              <div className="preview"></div>
+        <div className="right">
+          <div className="preview-wrapper">
+            <div className="preview">
+              {data.map(item => (
+                <div
+                  className={`pane ${
+                    activeId === item.id
+                      ? ''
+                      : activeId > item.id
+                      ? 'pane-after'
+                      : 'pane-before'
+                  }`}
+                ></div>
+              ))}
             </div>
           </div>
         </div>
       </div>
       <style jsx>{`
-        .wrapper {
-          width: 100vw;
-          height: 100vh;
-        }
-
         .container {
           position: relative;
           width: 100%;
           max-width: 1400px;
           margin: 0 auto;
           display: flex;
+          flex-direction: column-reverse;
           gap: 32px;
+          padding: 0 32px;
+        }
+
+        @media (min-width: 1200px) {
+          .container {
+            flex-direction: row;
+          }
         }
 
         .left {
-          width: 40%;
+          width: 100%;
         }
 
-        .feature {
-          padding: 16px 0 32px 0;
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: stretch;
+        @media (min-width: 1200px) {
+          .left {
+            width: 40%;
+          }
         }
 
         .right {
-          width: 60%;
+          width: 80%;
           position: sticky;
-          top: 0;
+          top: 32px;
           display: flex;
           flex-direction: column;
           justify-content: center;
-          height: 100vh;
+        }
+
+        @media (min-width: 1200px) {
+          .right {
+            width: 60%;
+            height: 100vh;
+            top: 0;
+          }
         }
 
         .preview-wrapper {
@@ -136,21 +171,50 @@ const Page = props => {
           display: block;
           width: 100%;
           height: 100%;
-          background: black;
-          // transform: translateY(-50%);
         }
 
-        h2 {
-          font-size: 54px;
-          line-height: 1.25;
-          font-weight: 600;
-          margin-bottom: 32px;
+        .pane {
+          position: absolute;
+          display: block;
+          width: 100%;
+          height: 100%;
+          border-radius: 10px;
+          background: #fff;
+          border: 1px solid #d1faf6;
+          box-shadow: 16px 16px 32px rgba(155, 155, 155, 0.1);
+          transition: all 0.5s ease-out;
+          opacity: 1;
         }
 
-        p {
-          font-size: 22px;
-          line-height: 1.8;
-          font-weight: 400;
+        .pane-after {
+          transform: translate3d(0, -100vh, 0);
+          transition: all 0.5s ease-in;
+          opacity: 0;
+        }
+
+        .pane-before {
+          transform: translate3d(0, 100vh, 0);
+          opacity: 0;
+        }
+      `}</style>
+    </>
+  )
+}
+
+const Page = props => {
+  return (
+    <>
+      <div className="wrapper">
+        <Story />
+        <div className="other-section"></div>
+        <Story />
+      </div>
+      <style jsx>{`
+        .wrapper {
+          width: 100vw;
+          height: 100vh;
+          background: #f2fdfc;
+          overflow-y: auto;
         }
 
         .other-section {
